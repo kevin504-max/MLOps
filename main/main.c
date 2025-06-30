779438b6fa30645b9bc7414f593bec326a2671ea
kevin504-max <kevinbrito2012@gmail.com>
2025-06-30 17:16:07 -0300

    feat: include tratative

--- main/main.c
+++ main/main.c
@@ -33,7 +33,14 @@
 #include "esp_sntp.h"
 
 void app_main() {
-    nvs_flash_init();
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_LOGI(TAG, "NVS Flash initialized successfully");
+
     wifi_connect();
     initialize_sntp();
     if (!wait_for_time_sync()) {
